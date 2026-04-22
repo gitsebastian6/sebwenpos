3f8b310bfecd5ede2ea912406295f412cc4e698b
Z User <z@container>
2026-04-22 21:22:29 +0000

    5a27392d-f888-4be3-a635-1c33d737ec85

--- _tmp_query.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-(async () => {
-  try {
-    const r = await p.paymentReceipt.findMany({ where: { storeId: 6 }, orderBy: { id: 'desc' }, take: 5 });
-    console.log(JSON.stringify(r, null, 2));
-  } catch(e) { console.error(e.message); }
-  await p.$disconnect();
-})();
