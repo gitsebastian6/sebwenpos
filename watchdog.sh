0878f10e53831a4a6d34bd0a83e40733938899a4
Z User <z@container>
2026-04-23 14:52:55 +0000

    11cbe07c-9664-46f6-a44c-9e18d6ec09fc

--- watchdog.sh
+++ watchdog.sh
@@ -2,9 +2,8 @@
 cd /home/z/my-project
 while true; do
   if ! ss -tlnp 2>/dev/null | grep -q ':3000.*LISTEN'; then
-    rm -rf .next
-    NEXT_TELEMETRY_DISABLED=1 NODE_OPTIONS="--max-old-space-size=1024" nohup npx next dev -p 3000 > /home/z/my-project/dev.log 2>&1 &
-    sleep 15
+    echo "[$(date)] Restarting..." >> /home/z/my-project/dev.log
+    NEXT_TELEMETRY_DISABLED=1 nohup npx next start -p 3000 >> /home/z/my-project/dev.log 2>&1 &
   fi
-  sleep 3
+  sleep 5
 done
