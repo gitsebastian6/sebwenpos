a50890b8d9b78baca3e53dd69e4c435b57b261bd
Z User <z@container>
2026-04-23 14:49:12 +0000

    db0175af-51dd-4cb4-8139-ec566f422cab

--- watchdog.sh
+++ watchdog.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cd /home/z/my-project
 while true; do
-  if ! ss -tlnp | grep -q ':3000.*LISTEN'; then
-    echo "[$(date)] Port 3000 down, restarting..." >> /home/z/my-project/dev.log
-    cd /home/z/my-project
-    npx next start -p 3000 >> /home/z/my-project/dev.log 2>&1 &
+  if ! ss -tlnp 2>/dev/null | grep -q ':3000.*LISTEN'; then
+    rm -rf .next
+    NEXT_TELEMETRY_DISABLED=1 NODE_OPTIONS="--max-old-space-size=1024" nohup npx next dev -p 3000 > /home/z/my-project/dev.log 2>&1 &
+    sleep 15
   fi
-  sleep 5
+  sleep 3
 done
